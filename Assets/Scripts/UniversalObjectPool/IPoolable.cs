ed215be5664b03869dd4a58bc84f7e9c6c43b483
Lyta Mahjoub <65280910+cutie-lyta@users.noreply.github.com>
2025-01-06 14:19:25 +0100

    Added Universal object pool

---
+++ Assets/Scripts/UniversalObjectPool/IPoolable.cs
@@ -0,0 +1,35 @@
+﻿
+using System.Reflection;
+using UnityEngine;
+
+/// <summary>
+/// The base interface for every object that can be in the object pool
+/// </summary>
+public interface IPoolable
+{
+    /// <summary>
+    /// Reimplementation of Unity's default gameObject field.
+    /// Will automatically get filled by MonoBehaviour inheritance
+    /// </summary>
+    public GameObject gameObject {get;}
+
+    /// <summary>
+    /// Register this object type into the object pool, to be sure the object pool can instantiate that kind of object.
+    /// Should get executed only if the current type hasn't been registered.
+    /// You can use this snippet to make sure the type isn't already registered:
+    /// <code> if (ObjectPoolManager.Instance.IsRegistered(GetType())) return; </code>
+    /// </summary>
+    public void RegisterType();
+    
+    /// <summary>
+    /// Function that get called when the object get pooled
+    /// It's basically the Object Pool equivalent of an Awake/Start/OnEnable
+    /// </summary>
+    public void OnPooled(string tag);
+    
+    /// <summary>
+    /// Function that get called when the object get unpooled
+    /// It's basically the Object Pool equivalent of an OnDestroy/OnDisable
+    /// </summary>
+    public void OnUnPooled();
+}
---
+++ Assets/Scripts/UniversalObjectPool/ObjectPoolManager.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+//TODO: A custom property drawer for those dictionnary
+public class ObjectPoolManager : MonoBehaviour
+{
+    /// <summary>
+    /// Singleton instance of this manager
+    /// </summary>
+    public static ObjectPoolManager Instance { get; private set; }
+    
+    /// <summary>
+    /// A dictionnary containing the type of the IPoolable object and a max number of instance.
+    /// It can accept non IPoolable class, but it will never get used,
+    /// because the type check is used at instanciation and pooling
+    /// </summary>
+    [Tooltip("The name of the type and the number max of instance of that type")]
+    [SerializeField] private SerializedDictionary<string, int> _maxValue;
+    
+    /// <summary>
+    /// A dictionnary containing the type of the IPoolable object and a prefab/gameobject to instanciate
+    /// whenever there is not enough object in the pool and in existance.
+    /// It can accept non IPoolable class, but it will never get used,
+    /// because the type check is used at instanciation and pooling
+    /// </summary>
+    [Tooltip("The name of the type and the prefab of that type")]
+    [SerializeField] private SerializedDictionary<string, GameObject> _prefabPool;
+    
+    /// <summary>
+    /// The current number of each type in the pool actually (both instanciated and dormant)
+    /// </summary>
+    private Dictionary<string, int> _currentPoolCount = new ();
+    
+    /// <summary>
+    /// A list of every dormant object in the pool
+    /// </summary>
+    private Dictionary<string, List<IPoolable>> _pool;
+    
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+
+        _pool = new Dictionary<string, List<IPoolable>>();
+    }
+
+    /// <summary>
+    /// Check if the type T already has a prefab attached to it, for easy instanciation
+    /// </summary>
+    /// <param name="T">
+    /// Type of the IPoolable object
+    /// It can accept non IPoolable class, but it will never get used,
+    /// because the type check is used at instanciation and pooling
+    /// </param>
+    /// <returns> Whether the type T has a prefab attached or not </returns>
+    public bool IsRegistered(string T)
+    {
+        return _prefabPool.ContainsKey(T);
+    }
+
+    /// <summary>
+    /// Add the type T to the prefab pool
+    /// </summary>
+    /// <param name="T">
+    /// Type of the IPoolable object
+    /// It can accept non IPoolable class, but it will never get used,
+    /// because the type check is used at instanciation and pooling
+    /// </param>
+    /// <param name="prefab"> The gameobject that will be used as instanciation prefab. </param>
+    public void AddToPrefabPool(string T, GameObject prefab)
+    {
+        prefab.transform.SetParent(transform);
+        _prefabPool.Add(T, prefab);
+    }
+
+    /// <summary>
+    /// Get an object of type T, which is an IPoolable.
+    /// </summary>
+    /// <typeparam name="T">The type of the object being return,
+    /// which should be a IPoolable MonoBehaviour subclass</typeparam>
+    /// <returns> The object being pooled </returns>
+    public IPoolable Pool(string item)
+    {
+        IPoolable pooledObject;
+        if (!_pool.ContainsKey(item) || _pool[item].Count == 0)
+        {
+            if (!_currentPoolCount.ContainsKey(item)) _currentPoolCount.Add(item, 0);
+            if (_currentPoolCount[item] >= _maxValue[item]) return null;
+
+            _currentPoolCount[item]++;
+            
+            if(!_pool.ContainsKey(item)) _pool.Add(item, new List<IPoolable>());
+
+            var prefab = _prefabPool[item];
+            var instance = Instantiate(prefab);
+            pooledObject = instance.GetComponent<IPoolable>();
+        }
+        else pooledObject = _pool[item][0];
+
+        _pool[item].Remove(pooledObject);
+        
+        pooledObject.gameObject.SetActive(true);
+        pooledObject.gameObject.transform.SetParent(null);
+        pooledObject.OnPooled(item);
+
+        return pooledObject;
+    }
+
+    /// <summary>
+    /// Store an object into the pool
+    /// </summary>
+    /// <param name="pooledObject"> The object to store </param>
+    public void Unpool(IPoolable pooledObject, string itemType)
+    {
+        pooledObject.gameObject.SetActive(false);
+        _pool[itemType].Add(pooledObject);
+    }
+}
