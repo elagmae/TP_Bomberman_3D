a10e82979453d4c2d68a09690c15aada5e71e578
elagmae <lea.pouthier@gmail.com>
2025-01-14 12:47:50 +0100

    added scene transitions

--- Assets/Scripts/SceneManagerBehaviour.cs
+++ Assets/Scripts/SceneManagerBehaviour.cs
@@ -1,20 +1,31 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class SceneManagerBehaviour : MonoBehaviour
 {
+    [SerializeField]
+    private Animator _transition;
     public void LoadScene(string sceneName)
     {
         Time.timeScale = 1f;
+        _transition.SetTrigger("Start");
+
+        StartCoroutine(WaitAndLoad(sceneName));
+    }
+
+    public IEnumerator WaitAndLoad(string sceneName)
+    {
+        yield return new WaitForSeconds(2.5f);
         SceneManager.LoadScene(sceneName);
     }
 
     public void Reload()
     {
         Time.timeScale = 1f;
-
+        _transition.SetTrigger("Start");
         var currentScene = SceneManager.GetActiveScene().name;
-        SceneManager.LoadSceneAsync(currentScene);
+        StartCoroutine(WaitAndLoad(currentScene));
     }
 
     public void Exit()
