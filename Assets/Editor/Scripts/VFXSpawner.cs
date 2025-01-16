e3d1d24f61fcf57c2ea5c94cd500d25bd7746e9b
Lyta Mahjoub <65280910+cutie-lyta@users.noreply.github.com>
2025-01-16 12:50:36 +0100

    Added asmdef

---
+++ Assets/Editor/Scripts/VFXSpawner.cs
@@ -0,0 +1,21 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Editor.Scripts
+{
+    public class VFXSpawner : EditorWindow
+    {
+        [MenuItem("MENUITEM/MENUITEMCOMMAND")]
+        private static void ShowWindow()
+        {
+            var window = GetWindow<VFXSpawner>();
+            window.titleContent = new GUIContent("TITLE");
+            window.Show();
+        }
+
+        private void CreateGUI()
+        {
+            
+        }
+    }
+}
