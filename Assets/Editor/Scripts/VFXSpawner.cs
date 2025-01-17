165465f756089b09ceac4c93e1bd86ec36daa9b3
Lyta Mahjoub <65280910+cutie-lyta@users.noreply.github.com>
2025-01-17 13:41:27 +0100

    Added custom WIndow and Tests

--- Assets/Editor/Scripts/VFXSpawner.cs
+++ Assets/Editor/Scripts/VFXSpawner.cs
@@ -1,18 +1,51 @@
-﻿using UnityEditor;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.VFX;
 
 public class VFXSpawner : EditorWindow
 {
-    [MenuItem("MENUITEM/MENUITEMCOMMAND")]
+    private GameObject _VFXold;
+    private VisualEffectAsset _VFXnew;
+
+    private bool _toggle;
+    
+    [MenuItem("Window/VFX Spawner")]
     private static void ShowWindow()
     {
         var window = GetWindow<VFXSpawner>();
-        window.titleContent = new GUIContent("TITLE");
+        window.titleContent = new GUIContent("VFX");
         window.Show();
     }
 
-    private void CreateGUI()
+    private void OnGUI()
     {
-            
+        _toggle = EditorGUILayout.Toggle("Is it a VFX Graph?", _toggle);
+        if (_toggle)
+        {
+            _VFXnew = EditorGUILayout.ObjectField("VFX new", _VFXnew, typeof(VisualEffectAsset), false) as VisualEffectAsset;
+        }
+        else {
+            _VFXold = EditorGUILayout.ObjectField("VFX old", _VFXold, typeof(GameObject), false) as GameObject;
+        }
+
+        if (GUILayout.Button("Spawn VFX"))
+        {
+            foreach (GameObject obj in Selection.gameObjects)
+            {
+                if (_toggle)
+                {
+                    GameObject child = new GameObject();
+                    var vfx = obj.AddComponent<VisualEffect>();
+                    vfx.visualEffectAsset = _VFXnew;
+                    
+                    child.transform.SetParent(obj.transform);
+                }
+                else
+                {
+                    var go = Instantiate(_VFXold, obj.transform, false);
+                }
+            }
+        }
     }
 }
+
