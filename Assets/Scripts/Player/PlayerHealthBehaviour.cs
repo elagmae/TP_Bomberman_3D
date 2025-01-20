5de6a9566f7413b25605872a78c68d18247f5ed3
elagmae <lea.pouthier@gmail.com>
2025-01-20 15:34:26 +0100

    deleted health debug

--- Assets/Scripts/Player/PlayerHealthBehaviour.cs
+++ Assets/Scripts/Player/PlayerHealthBehaviour.cs
@@ -5,21 +5,10 @@ public class PlayerHealthBehaviour : MonoBehaviour
 {
     public event Action<int, int> OnHealthChange;
     private PlayerMain _main;
-    [SerializeField]
-    private bool _lostHealthDebug;
 
     private void Start()
     {
         _main = GetComponent<PlayerMain>();
-    }
-
-    private void Update()
-    {
-        if( _lostHealthDebug)
-        {
-            _lostHealthDebug = false;
-            LooseHealth();
-        }
     }
 
     public void LooseHealth()
